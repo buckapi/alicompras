7db589acaf0aad26e8f823e17489debbe36da456
root <root@localhost.localdomain>
2025-01-10 10:39:25 -0500

    enlaces

--- src/app/componentes/ui/head/head.component.ts
+++ src/app/componentes/ui/head/head.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { GlobalService } from '../../../services/global.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-head',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './head.component.html',
   styleUrl: './head.component.css'
 })
 export class HeadComponent {
+  isMenuOpen = false;
+  showMenu() {
+    this.isMenuOpen = true;
+  }
+    toggleMenu() {
+        this.isMenuOpen = !this.isMenuOpen;
+    }
 constructor(
   public global: GlobalService
 ){}
